306ab261b76d453b76b5e12f0039cbc6ae21ae10
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-11 12:47:40 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int d = scanner.nextInt();
+        for (int a = 0; a <=10; a++ ) {
+            System.out.println(d + "*"+a+"="+d*a +"\t\t");
+    }
+    }
+}
